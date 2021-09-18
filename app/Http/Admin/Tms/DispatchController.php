c492c9afdbd911a728e7c7d78911d07c03c0c11e
chitushgyl <chitushgyl@163.com>
2021-09-18 14:32:06 +0800

    查询微信支付结果

--- app/Http/Admin/Tms/DispatchController.php
+++ app/Http/Admin/Tms/DispatchController.php
@@ -261,7 +261,7 @@ class DispatchController extends CommonController{
                 ['delete_flag','=','Y'],
                 ['receiver_id','=',$group_code],
             ];
-            $select=['self_id','receiver_id','company_name','create_time','create_time','group_name','group_code','order_type',
+            $select=['self_id','receiver_id','company_name','create_time','create_time','group_name','group_code','order_type','order_id',
                 'gather_sheng_name','gather_shi_name','gather_qu_name','gather_address','order_status','car_type','receipt_flag',
                 'send_sheng_name','send_shi_name','send_qu_name','send_address','clod','on_line_money','company_id','total_user_id',
                 'good_info','good_number','good_weight','good_volume','total_money','send_time','gather_time','pay_type','pay_status'];
