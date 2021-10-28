30c3b18980e1f7e48f630565200efdfd53cd1aa3
chitushgyl <chitushgyl@163.com>
2021-10-28 09:45:49 +0800

    商品规格设置长度

--- app/Http/Admin/Wms/GoodController.php
+++ app/Http/Admin/Wms/GoodController.php
@@ -447,8 +447,8 @@ class GoodController extends CommonController{
              */
 
             $shuzu=[
-                '产品编号' =>['Y','N','64','external_sku_id'],
-                '产品名称' =>['Y','Y','255','good_name'],
+                '商品编号' =>['Y','N','64','external_sku_id'],
+                '商品名称' =>['Y','Y','255','good_name'],
                 '商品英文名称' =>['N','Y','255','good_english_name'],
                 '规格' =>['N','Y','50','wms_spec'],
                 '主计量单位' =>['N','Y','10','wms_unit'],
