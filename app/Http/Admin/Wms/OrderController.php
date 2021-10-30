7c8c956d8f9ffee3469c13018ce66c987646be09
chitushgyl <chitushgyl@163.com>
2021-10-30 19:29:20 +0800

    商品规格设置长度

--- app/Http/Admin/Wms/OrderController.php
+++ app/Http/Admin/Wms/OrderController.php
@@ -253,25 +253,25 @@ class OrderController extends CommonController{
 
 			$order_check    =array_count_values($order_check);
 //            dd($order_check);
-            foreach ($last_names as $key =>$value){
-                $where_shop1=[
-                    ['delete_flag','=','Y'],
-                    ['external_id','=',$key],
-                    ['company_id','=',$company_id],
-                ];
+//            foreach ($last_names as $key =>$value){
+//                $where_shop1=[
+//                    ['delete_flag','=','Y'],
+//                    ['external_id','=',$key],
+//                    ['company_id','=',$company_id],
+//                ];
+//
+////                dd($where_shop1);
+//                $select_wmsShop1=['self_id','group_code','external_id','name','contacts','address','tel','group_name','company_id','company_name'];
+//                $shop_info2 = wmsShop::where($where_shop1)->select($select_wmsShop1)->first();
+//                if(empty($shop_info2)){
+//                    if($abcd<$errorNum){
+//                        $strs .= '数据中的第'.($value+2)."行门店编码不存在".'</br>';
+//                        $cando='N';
+//                        $abcd++;
+//                    }
+//                }
+//            }
 
-//                dd($where_shop1);
-                $select_wmsShop1=['self_id','group_code','external_id','name','contacts','address','tel','group_name','company_id','company_name'];
-                $shop_info2 = wmsShop::where($where_shop1)->select($select_wmsShop1)->first();
-                if(empty($shop_info2)){
-                    if($abcd<$errorNum){
-                        $strs .= '数据中的第'.($value+2)."行门店编码不存在".'</br>';
-                        $cando='N';
-                        $abcd++;
-                    }
-                }
-            }
-            dd($order_num);
             foreach($order_num as $k => $v){
 				//dd($v);
                 $where_shop=[
