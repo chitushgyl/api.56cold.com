71ea05e63b9695f7f9abb5f1c286a407c9a8093b
chitushgyl <chitushgyl@163.com>
2021-10-30 21:35:26 +0800

    商品规格设置长度

--- app/Http/Admin/Wms/OrderController.php
+++ app/Http/Admin/Wms/OrderController.php
@@ -277,7 +277,7 @@ class OrderController extends CommonController{
             $select_wmsShop=['self_id','group_code','external_id','name','contacts','address','tel','delete_flag','group_name','company_id','company_name','contacts_code'];
             DB::connection()->enableQueryLog();
             wmsShop::where($where_shop2)->select($select_wmsShop)->first();
-            dump(DB::getQueryLog());
+//            dump(DB::getQueryLog());
             foreach($order_num as $k => $v){
                 $where_shop=[
                     ['delete_flag','=','Y'],
@@ -287,7 +287,7 @@ class OrderController extends CommonController{
                 ];
                 dump($where_shop);
                 $select_wmsShop=['self_id','group_code','external_id','name','contacts','address','tel','delete_flag','group_name','company_id','company_name','contacts_code'];
-                $shop_info = wmsShop::query()->where($where_shop)->select($select_wmsShop)->toSql();
+                $shop_info = wmsShop::where($where_shop)->select($select_wmsShop)->first();
                 dump(DB::getQueryLog());
                 if($cando == 'Y'){
                     $order_2=[];
