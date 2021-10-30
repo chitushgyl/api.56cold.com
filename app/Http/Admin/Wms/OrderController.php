351121ec14cc70fa30a56de5fd186e8736b903aa
chitushgyl <chitushgyl@163.com>
2021-10-30 18:12:52 +0800

    商品规格设置长度

--- app/Http/Admin/Wms/OrderController.php
+++ app/Http/Admin/Wms/OrderController.php
@@ -247,10 +247,10 @@ class OrderController extends CommonController{
 
 			$last_names		=array_flip(array_unique($order_check));
 
-//			dump($order_check);dump(array_unique($order_check));dump($last_names);
+			dump($order_check);dump(array_unique($order_check));dump($last_names);
 
 			$order_check    =array_count_values($order_check);
-//            dd($order_check);
+            dd($order_check);
             foreach ($last_names as $key =>$value){
                 $where_shop1=[
                     ['delete_flag','=','Y'],
