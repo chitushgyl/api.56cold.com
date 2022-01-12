641ef764010b8ee9b778fc223dbaef1fb54e7829
chitushgyl <chitushgyl@163.com>
2022-01-12 16:52:04 +0800

    修改上架

--- app/Http/Admin/Wms/LibraryController.php
+++ app/Http/Admin/Wms/LibraryController.php
@@ -1041,7 +1041,7 @@ class LibraryController extends CommonController{
         $select=['self_id','grounding_status','order_status','type','company_name','create_user_name','create_time','group_name','check_user_name','check_time','grounding_status','count','voucher','type','warehouse_id','warehouse_name'];
 
 		$WmsLibrarySigeSelect=[
-            'self_id','grounding_status',
+            'self_id','grounding_status','bulk','weight',
 		    'order_id',
             'external_sku_id','good_name','spec','production_date','expire_time','initial_num as now_num','good_unit','good_target_unit','good_scale',
             'area','row','column','tier',
