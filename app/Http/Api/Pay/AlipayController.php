dd31d533aea6c169ecda18f793616831aaea2ca7
chitushgyl <chitushgyl@163.com>
2022-03-05 09:23:55 +0800

    测试

--- app/Http/Api/Pay/AlipayController.php
+++ app/Http/Api/Pay/AlipayController.php
@@ -2013,14 +2013,12 @@ class AlipayController extends Controller{
     public function fastOrderBalancePay(Request $request){
         $input = $request->all();
         $user_info = $request->get('user_info');//接收中间件产生的参数
+
+        if (!$user_info){
             $msg['code'] = 401;
-            $msg['msg']  = $user_info;
+            $msg['msg']  = '未登录，请完成登录！';
             return $msg;
-//        if (!$user_info){
-//            $msg['code'] = 401;
-//            $msg['msg']  = '未登录，请完成登录！';
-//            return $msg;
-//        }
+        }
         // 订单ID
         $self_id = $request->input('self_id');
         // 支付金额
