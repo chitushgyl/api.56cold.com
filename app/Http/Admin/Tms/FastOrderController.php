fbc23ebf4b589382eb4f4f3afcd396e742d532c5
chitushgyl <chitushgyl@163.com>
2022-06-10 15:18:01 +0800

    货到付款支付按钮

--- app/Http/Admin/Tms/FastOrderController.php
+++ app/Http/Admin/Tms/FastOrderController.php
@@ -313,7 +313,7 @@ class FastOrderController extends CommonController{
 
         $where=get_list_where($search);
 
-        $select=['self_id','group_name','create_user_name','create_time','use_flag','order_type','order_status','clod','good_info',
+        $select=['self_id','group_name','create_user_name','create_time','use_flag','order_type','order_status','clod','good_info','pay_status',
             'gather_address_id','gather_contacts_id','gather_name','gather_tel','gather_sheng','gather_shi','gather_qu','gather_qu_name','gather_address',
             'send_address_id','send_contacts_id','send_name','send_tel','send_sheng','send_shi','send_qu','send_qu_name','send_address','total_money','pay_type',
             'good_name','good_number','good_weight','good_volume','gather_shi_name','send_shi_name','gather_time','send_time'];
