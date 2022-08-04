c35de99998762fa137d52196d3634479f456cfd4
chitushgyl <chitushgyl@163.com>
2022-08-04 10:47:56 +0800

    司机端调度信息

--- app/Http/Api/Tms/OrderController.php
+++ app/Http/Api/Tms/OrderController.php
@@ -2378,14 +2378,14 @@ class OrderController extends Controller{
             $order_details8['name'] = '时效';
             $order_details8['value'] = $info->trunking;
             $order_details8['color'] = '#000000';
-//            if ($info->tmsOrderDispatch[0]["tmsCarriageDispatch"])
-//            if($info->tmsOrderDispatch[0]["tmsCarriageDispatch"]->tmsCarriage[0]['carriage_flag'] == 'carriers'){
-//                $order_details9['name'] = '调度信息';
-//                $order_details9['value'] = $info->car_info;
-//            }else{
-                $order_details9['name'] = '运输信息';
-                $order_details9['value'] = $info->car_info;
-//            }
+
+            $order_details9['name'] = '运输信息';
+            $order_details9['value'] = $info->car_info;
+            if ($info->tmsOrderDispatch[0]["tmsCarriageDispatch"]){
+                if($info->tmsOrderDispatch[0]["tmsCarriageDispatch"]->tmsCarriage[0]['carriage_flag'] == 'carriers'){
+                    $order_details9['name'] = '调度信息';
+                }
+            }
 
             $order_details10['name'] = '回单信息';
             $order_details10['value'] = $info->receipt;
