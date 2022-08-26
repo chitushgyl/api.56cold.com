b72db95a9119a7802014a4ed52cb470e11c45657
chitushgyl <chitushgyl@163.com>
2022-08-26 14:12:24 +0800

    获取轮播图

--- app/Http/Admin/Wms/SearchController.php
+++ app/Http/Admin/Wms/SearchController.php
@@ -563,7 +563,7 @@ class SearchController extends CommonController{
 
 
         }
-
+            dd($old_change,$new_change);
             if($id){
                 $change->change($old_change,'moveout');
                 $change->change($new_change,'movein');
