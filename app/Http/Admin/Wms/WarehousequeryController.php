ac6a30b89a1c4db31d4d157623553ec65907d0a3
chitushgyl <chitushgyl@163.com>
2023-01-17 18:14:27 +0800

    1

--- app/Http/Admin/Wms/WarehousequeryController.php
+++ app/Http/Admin/Wms/WarehousequeryController.php
@@ -202,7 +202,7 @@ class WarehousequeryController extends CommonController{
                               $data['warehouse_info']->maintain++;
 
                           }else{
-                             if($vv->wmsLibrarySige){
+                             if(count($vv->wmsLibrarySige)>0){
                                  $vv->status = 'Y';
                                  $v->count++;
                                  $v->use_count++;
